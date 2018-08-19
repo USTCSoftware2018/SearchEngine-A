15600a2a1a407ed53887ae77d283d307caddb440
ForeverFancy <zhangbowen@mail.ustc.edu.cn>
2018-08-19 10:41:31 +0800

    Return List

--- search_engine_for_ghr_gene.py
+++ search_engine_for_ghr_gene.py
@@ -1,4 +1,5 @@
 import requests
+import json
 from bs4 import BeautifulSoup
 # Author:BwZhang
 
@@ -6,7 +7,7 @@ from bs4 import BeautifulSoup
 class Search():
 
     def __init__(self,term):
-        self.term=term
+        self.term = term
 
     def search_info(self):
         url = ' https://ghr.nlm.nih.gov/search?show=xml&count=1&query=' + self.term   # only search the top result
@@ -14,17 +15,30 @@ class Search():
         soup = BeautifulSoup(webdata, 'lxml')
         results = soup.select('result')
 
-        with open('summary.txt','a+') as f:
-            for result in results:
-                for tag in result.find_all(True):
-                    if len(tag.contents) == 1:
-                        if tag.name == 'symbol' or tag.name == 'name':
-                            f.write('gene ' + tag.name + ':' + tag.get_text()+'\n')
-                        else:
-                            f.write(tag.name + ':' + tag.get_text()+'\n')
-                f.write('\n')
+        results_list = []
+
+        for result in results:
+            detail = {}
+            detail['dbname'] = 'ghr_gene'
+            for tag in result.find_all(True):
+                if len(tag.contents) == 1:
+                    if tag.name == 'name':
+                        detail['gene name'] = tag.get_text()
+                        temp_detail = json.dumps(detail)
+                        join_detail = json.loads(temp_detail)
+                        results_list.append(join_detail)
+                        detail.clear()
+
+                    elif tag.name == 'symbol':
+                        detail['gene symbol'] = tag.get_text()
+
+                    else:
+                        detail[tag.name] = tag.get_text()
+
+        return results_list
 
 
-if __name__=='__main__':
-    item=Search('ABC')
-    item.search_info()
+if __name__ == '__main__':
+    item = Search('ABC')
+    result = item.search_info()
+    print(result)
